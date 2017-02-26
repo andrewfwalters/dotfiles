8a3c7f702494fc86e4d564ae3c44d3c4212aac31
Andrew Walters <andrew@andrewwalters.me>
2017-02-26 03:14:17 +0000

    initial commit

---
+++ make_links.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+# link dotfiles to users home area
+# author: andrew
+# inputs: none
+
+ln -sf ~/dotfiles/andrew.bashrc_custom ~/.bashrc_custom
+ln -sf ~/dotfiles/andrew.screenrc ~/.screenrc
+ln -sf ~/dotfiles/andrew.profile ~/.profile
+ln -sf ~/dotfiles/andrew.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/andrew.nanorc ~/.nanorc
+ln -sf ~/dotfiles/andrew.bashrc ~/.bashrc
